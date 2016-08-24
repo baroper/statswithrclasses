7ab13873b202b0e938a39d406c5e4bad4d956604
“baroper” <baroper1@gmail.comgit config --global user.name baropergit config remote.origin.url git@github.com:baroper/statswithrclasses.gitgit remote add origin https://github.com/baroper/statswithrclasses.gitgit config remote.origin.url git@github.com:baroper/statswithrclasses.gitgit pull -u origin mastergit push -u origin masterhttps://github.com/baroper/statswithrclasses.ggit config --global user.email baroper1@gmail.com>
2016-08-24 15:07:54 -0700

    Just playing around

--- Explanation.R
+++ Explanation.R
@@ -4,3 +4,18 @@ library(ggplot2)
 library(dplyr)
 
 data("Orange")
+names(Orange)
+
+str(Orange)
+names(Orange)
+orangetree <- Orange$Tree
+sample(Orange$Tree,10,FALSE)
+print(Orange$Tree)
+sample(Orange$Tree,35,FALSE)
+treesample <- sample(Orange$Tree,35,FALSE)
+treesample
+count(treesample)
+median(treesample)
+str(treesample)
+orangeagesample <- sample(Orange$age,35,FALSE)
+orangeagesample
